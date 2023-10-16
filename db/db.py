b7d250558869d22e16e231bd1bb31304959df468
Ethan Bootehsaz <ethandb2024@gmail.com>
2023-10-15 22:35:20 -0400

    users DB setup

--- db/db.py
+++ db/db.py
@@ -1,12 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from datetime import datetime
+
 """
 This file will manage interactions with our data store.
 At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
 
+db = SQLAlchemy()
 
 def fetch_pets():
     """
     A function to return all pets in the data store.
     """
     return {"tigers": 2, "lions": 3, "zebras": 1}
+
+def init_app(app):
+    db.init_app(app)
+    migrate = Migrate(app, db)
---
+++ db/schemas.py
@@ -0,0 +1,8 @@
+from flask_marshmallow import Marshmallow
+from .models import User
+
+ma = Marshmallow()
+
+class UserSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = User
