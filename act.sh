c6f9c8e9328df2de6bf6c358a9034718a0bac39f
El <elijah.d.whittle@gmail.com>
2023-10-22 22:33:41 -0400

    added option to store API key in a text file

--- act.sh
+++ act.sh
@@ -1,4 +1,15 @@
 #/bin/bash
+
+API_KEY_FILE="api_key.txt" # put your API key in a file named api_key.txt
+
 source .venv/bin/activate
 # to get this venv on your own: 
 # $ python -m venv .venv
+
+if [ -f "$API_KEY_FILE" ]
+then
+  export NEWS_API_KEY="$(cat $API_KEY_FILE)";
+  echo "News API key stored in NEWS_API_KEY."
+else
+  echo "could not find your news API key."
+fi
