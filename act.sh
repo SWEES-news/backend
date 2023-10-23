9794d5f43d0f969a629b640b52faecabca87ec6e
El <elijah.d.whittle@gmail.com>
2023-10-22 22:49:33 -0400

    updated steps to include optional act.sh

--- act.sh
+++ act.sh
@@ -1,8 +1,9 @@
 #/bin/bash
 
+PYENV_DIR=".venv"
 API_KEY_FILE="api_key.txt" # put your API key in a file named api_key.txt
 
-source .venv/bin/activate
+source $PYENV_DIR/bin/activate
 # to get this venv on your own: 
 # $ python -m venv .venv
 
@@ -11,5 +12,5 @@ then
   export NEWS_API_KEY="$(cat $API_KEY_FILE)";
   echo "News API key stored in NEWS_API_KEY."
 else
-  echo "could not find your news API key."
+  echo "Could not find your news API key. Store it at the root directory in a file named $API_KEY_FILE."
 fi
