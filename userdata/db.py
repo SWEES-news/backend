6ff72d99b0d255ccc8a3069ce2bb3308ce096f58
Ethan Bootehsaz <ethandb2024@gmail.com>
2023-11-12 17:18:32 -0500

    added pymongo connection test

--- userdata/db.py
+++ userdata/db.py
@@ -4,6 +4,12 @@ At first, it will just contain stubs that return fake data.
 Gradually, we will fill in actual calls to our datastore.
 """
 import random
+import os
+
+import pymongo as pm
+
+LOCAL = "0"
+CLOUD = "1"
 
 ID_LEN = 24
 BIG_NUM = 100000000000000000000
@@ -23,6 +29,39 @@ MAX_MOCK_LEN = MAX_EMAIL_LEN - EMAIL_TAIL_LEN
 users = {
     MOCK_EMAIL: {NAME: MOCK_NAME, PASSWORD: MOCK_PASSWORD}
 }
+
+MONGO_ID = '_id'
+
+
+def connect_db():
+    """
+    This provides a uniform way to connect to the DB across all uses.
+    Returns a mongo client object... maybe we shouldn't?
+    Also set global client variable.
+    We should probably either return a client OR set a
+    client global.
+    """
+    global client
+    if client is None:  # not connected yet!
+        print("Setting client because it is None.")
+        if os.environ.get("CLOUD_MONGO", LOCAL) == CLOUD:
+            password = os.environ.get("GAME_MONGO_PW")
+            if not password:
+                raise ValueError('You must set your password '
+                                 + 'to use Mongo in the cloud.')
+            print("Connecting to Mongo in the cloud.")
+            client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
+                                    + '@cluster0.eqxbbqd.mongodb.net/'
+                                    + '?retryWrites=true&w=majority')
+            # PA recommends these settings:
+            # + 'connectTimeoutMS=30000&'
+            # + 'socketTimeoutMS=None
+            # + '&connect=false'
+            # + 'maxPoolsize=1')
+            # but they don't seem necessary
+        else:
+            print("Connecting to Mongo locally.")
+            client = pm.MongoClient()
 
 
 # returns json of mock user
