e09c8fe540279f581d581a0fda398423a4c835d0
Sanjana Gupta <sg6575@nyu.edu>
2024-02-12 00:02:54 -0500

    added proj_ideas/sentiment_model.py with resources for pre-trained models

---
+++ proj_ideas/sentiment_model.py
@@ -0,0 +1,8 @@
+# Use a pre-trained sentiment analysis AI/NLP model to analyze news articles
+# The below example uses FinBERT model
+
+# Download and load FinBert pretrained model according to this article: 
+# https://www.newscatcherapi.com/blog/sentiment-analysis-using-python
+
+# Other sentiment analysis pre-trained models: 
+# https://medium.com/@sharma.tanish096/sentiment-analysis-using-pre-trained-models-and-transformer-28e9b9486641
