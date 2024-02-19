281052b759afa9542d78eee0dc38c8e820a8e544
Sanjana Gupta <sg6575@nyu.edu>
2024-02-19 00:52:52 -0500

    more formatting adjustments for langchain_analysis.py

--- server/langchain_analysis.py
+++ server/langchain_analysis.py
@@ -1,15 +1,13 @@
 # Some starter code to work off of the basic principles
 # ...outlined in ai_LangChain_Atlas.md
 
-
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
 prompt = ChatPromptTemplate.from_template(
-    """Analyse the news article for bias towards the Republican 
-    or Democratic party: {article}"""
-)
+    """Analyse the news article for bias towards the Republican
+    or Democratic party: {article}""")
 
 # need to replace openai_api_key argument with an environment variable
 model = ChatOpenAI(openai_api_key="YOUR_API_KEY")
