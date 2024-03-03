ab0ffcb875bf7a70b3a44ee19858e4ba425781ed
Sina <sinafallah98@gmail.com>
2024-03-03 17:34:23 -0500

    added explanation and improvement to web_scrapping

--- ai/web_scrapping.py
+++ ai/web_scrapping.py
@@ -2,6 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 import textwrap
 
+"""
+To be improved:
+This is the initial approch that we are using. However, this method can not extract the text of
+paid articles, such as New York Times.
+so if the user is subscribed to those websites, we are not able to extraxt it using this code.
+We might need to come up with a different approcah.
+"""
+
 # A test URL of an article
 url = 'https://www.engadget.com/the-morning-after-apples-car-project-may-be-dead-121513763.html'
 
